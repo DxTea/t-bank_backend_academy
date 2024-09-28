ba7c143cd741f0a09ee055d832d0d4c3650f8be0
DxTea <dexter_010@mail.ru>
2024-09-28 05:44:18 +0500

    Keep only README.md in main branch

--- main.py
+++
@@ -1,7 +0,0 @@
-from scr.word_dictionary import WordDictionary
-from scr.game_loop import GameLoop
-
-if __name__ == "__main__":
-    word_dictionary = WordDictionary()
-    game_loop = GameLoop(word_dictionary)
-    game_loop.run()
